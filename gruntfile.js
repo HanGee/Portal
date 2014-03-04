51adc96811584acd0e5430d523f74a56f68da5ed
Ian Wang <gogoenwon@gmail.com>
2014-03-05 02:05:51 +0800

    base

---
+++ gruntfile.js
@@ -0,0 +1,100 @@
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+        'gh-pages': {
+            options: {
+                base: 'build'
+            },
+            src: '**/*'
+        },
+        copy: {
+            vendor: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'src/',
+                        src: 'public/images/**/*',
+                        dest: 'build/'
+                    },
+
+                ]
+            }
+        },
+        uglify: {
+            options: {
+                compress: false,
+                beautify: true,
+                mangle: false,
+                sourceMap: 'build/public/js/main.min.js.map'
+            },
+            target: {
+                files: {
+                    'build/public/js/main.min.js': ['src/public/js/*.js']
+                }
+            }
+        },
+        jade: {
+            compile: {
+                options:{
+                    pretty: true
+                },
+                files: {
+                    'build/index.html': 'src/views/index.jade'
+                }
+            }
+        },
+        sass: {
+            main: {
+                options: {
+                    compass: true
+                },
+                files: {
+                    'build/public/css/style.css': 'src/public/css/style.sass'
+                }
+            }
+        },
+        watch: {
+            options: {
+                livereload: true,
+            },
+            jade: {
+                files: ['src/views/**/*.jade'],
+                tasks: ['jade'],
+                options: {
+                    //
+                }
+            },
+            html:{
+                files: ['build/**/*.html']
+            },
+            sass: {
+                files: ['src/public/css/**/*.sass', 'src/public/css/**/*.scss'],
+                tasks: ['sass'],
+                options: {
+                    //
+                }
+            },
+            js: {
+                files: ['src/public/js/*.js'],
+                tasks: ['uglify'],
+                options: {
+                    //
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-gh-pages');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-jade');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    grunt.registerTask('publish', ['build', 'gh-pages']);
+
+    grunt.registerTask('default', ['build', 'watch']);
+
+    grunt.registerTask('build', ['uglify', 'copy', 'jade', 'sass']);
+
+};
