82187e076fb1406449386bea5547797fce33b2da
Ian Wang <gogoenwon@gmail.com>
2014-04-09 17:59:49 +0800

    base on node

---
+++ app.js
@@ -0,0 +1,31 @@
+
+var express = require('express');
+var routes = require('./routes');
+var http = require('http');
+var path = require('path');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+  app.locals.pretty = true;
+}
+
+app.get('/', routes.index);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('HanGee Portal is on port ' + app.get('port'));
+});
--- gruntfile.js
+++ gruntfile.js
@@ -1,54 +1,28 @@
 module.exports = function (grunt) {
 
     grunt.initConfig({
-        'gh-pages': {
-            options: {
-                base: 'build'
-            },
-            src: '**/*'
-        },
-        copy: {
-            vendor: {
-                files: [
-                    {
-                        expand: true,
-                        cwd: 'src/',
-                        src: 'public/img/**/*',
-                        dest: 'build/'
-                    },{
-                        expand: true,
-                        cwd: 'src/',
-                        src: 'vendor/**/css/**.css',
-                        dest: 'build/'
-                    },{
-                        expand: true,
-                        cwd: 'src/',
-                        src: ['vendor/**/*.min.js', 'vendor/**/*.min.map'],
-                        dest: 'build/'
-                    },
-                ]
-            }
-        },
         uglify: {
             options: {
                 mangle: false,
-                sourceMap: 'build/public/js/main.min.js.map'
+                sourceMap: 'public/build/js/main.min.js.map'
             },
             target: {
                 files: {
-                    'build/public/js/main.js': ['src/public/js/*.js']
+                    'public/build/js/main.js': ['public/js/*.js']
                 }
             }
         },
-        jade: {
-            compile: {
-                options:{
-                    pretty: true
-                },
-                files: {
-                    'build/index.html': 'src/views/index.jade'
-                }
-            }
+        copy: {
+          image: {
+            files: [
+              {
+                expand: true,
+                cwd: 'public/img',
+                src: '**.*{png,jpg,svg}',
+                dest: 'public/build/img/'
+              }
+            ]
+          }
         },
         sass: {
             main: {
@@ -56,7 +30,7 @@ module.exports = function (grunt) {
                     compass: true
                 },
                 files: {
-                    'build/public/css/style.css': 'src/public/css/style.sass'
+                    'public/build/css/style.css': 'public/css/style.sass'
                 }
             }
         },
@@ -64,25 +38,15 @@ module.exports = function (grunt) {
             options: {
                 livereload: true,
             },
-            jade: {
-                files: ['src/views/**/*.jade'],
-                tasks: ['jade'],
-                options: {
-                    //
-                }
-            },
-            html:{
-                files: ['build/**/*.html']
-            },
             sass: {
-                files: ['src/public/css/**/*.sass', 'src/public/css/**/*.scss'],
+                files: ['public/css/**/*.sass'],
                 tasks: ['sass'],
                 options: {
                     //
                 }
             },
             js: {
-                files: ['src/public/js/*.js'],
+                files: ['public/js/*.js'],
                 tasks: ['uglify'],
                 options: {
                     //
@@ -90,8 +54,7 @@ module.exports = function (grunt) {
             }
         },
         clean: {
-          build: ["build"],
-          vendor: ["src/vendor"]
+          build: ['public/build']
         },
         bower: {
           install: {
@@ -103,16 +66,13 @@ module.exports = function (grunt) {
         }
     });
 
-    grunt.loadNpmTasks('grunt-gh-pages');
-    grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-jade');
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-bower-task');
 
-    grunt.registerTask('publish', ['build', 'gh-pages']);
     grunt.registerTask('default', ['build', 'watch']);
-    grunt.registerTask('build', ['clean:build', 'bower', 'uglify', 'copy', 'jade', 'sass']);
+    grunt.registerTask('build', ['clean:build', 'bower', 'copy', 'uglify', 'sass']);
 };
